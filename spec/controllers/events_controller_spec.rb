6a604d1bd2537371bae9d5f394990989054b66d6
Spartan-626 <kwilliamson0232@gmail.com>
2020-01-22 02:54:03 +0000

    Adding validation to events

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -23,5 +23,11 @@ RSpec.describe EventsController, type: :controller do
       event = Event.last
       expect(event.description).to eq('Dungeons & Dragons Campaign Run')
     end
+
+    it "should properly deal with validation errors" do
+      post :create, params: { event: { description: '' } }
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(Event.count).to eq 0
+    end
   end
 end
