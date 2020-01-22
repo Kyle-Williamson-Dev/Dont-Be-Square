b491b67dcf907a8cc33efe7e1dc6e25c1e5e2238
Spartan-626 <kwilliamson0232@gmail.com>
2020-01-22 05:11:45 +0000

    Trying to add hero image to index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_20_004524) do
+ActiveRecord::Schema.define(version: 2020_01_22_025657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_01_20_004524) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
